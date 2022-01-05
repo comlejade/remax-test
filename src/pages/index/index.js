a8e5803ce4590348721b13acf653196561cc5afe
QAQ <thundersmile@126.com>
2022-01-05 20:33:50 +0800

    bug

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,12 +4,6 @@ import VanButton from '@vant/weapp/lib/button';
 import VanField from '@vant/weapp/lib/field';
 import styles from './index.css'
 
-/**
- * remax 版本升级到 2.15.11 后，button的样式还存在问题，但是事件不再影响
- * VanField 的输入问题目前没能复现，但是实际项目中依然存在问题，
- * 当我注释掉某些包含 VanField 的其他页面后，测试页的输入事件才不受影响
- * 感觉可能是编译后生成的公共组件中的内容会相互冲突
- */
 export default () => {
   const [value, setValue] = useState('')
 
@@ -17,7 +11,7 @@ export default () => {
     console.log(e.detail)
     setValue(e.detail)
   }
-  
+
   const handleClick = () => {
     console.log('1111')
   }
